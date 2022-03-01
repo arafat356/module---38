b231ad854945b270beae1ddb776080a5586c9393
arafat356 <ahmedarafatft@gmail.com>
2022-03-02 01:53:31 +0600

    nothing

--- alert.js
+++ alert.js
@@ -1,15 +1,24 @@
+// alert 
 console.log('i am here');
-// alert('ma is coming');
 const maComing = () => {
     alert('Mother is coming please dont use your phoen')
 };
-const askGoing = () => {
-    const response = confirm('Are you Coming with us');
-    console.log(response);
-    if (response === true) {
-        alert('Send the fee on my Account');
 
-    } else {
-        console.log('go from here and die')
+// response comfirm asking from user    
+const askGoing = () => {
+        const response = confirm('Are you Coming with us');
+        console.log(response);
+        if (response === true) {
+            alert('Send the fee on my Account');
+
+        } else {
+            console.log('go from here and die')
+        }
+    }
+    // ask name 
+const AskName = () => {
+    const name = prompt('What is your name!!');
+    if (name) {
+        console.log(name)
     }
 }
